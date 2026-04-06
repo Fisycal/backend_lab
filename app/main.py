68697fb0a09fe0063359155b709322cab2c7937c
Michael <physicalmichael2015@gmail.com>
2026-04-06 17:55:59 -0500

    Fix typo when adding logs to main.py

--- app/main.py
+++ app/main.py
@@ -12,7 +12,7 @@ import logging
 
 logging.basicConfig(
     level=logging.INFO,
-    format=%(asctime)s | %(levelname)s |%(name)s | %(message)s"
+    format="%(asctime)s | %(levelname)s |%(name)s | %(message)s"
 )
 
 logger = logging.getLogger(__name__)
